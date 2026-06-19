695592cc0419248c567262037c7d3229b45ef984
Ganesh R <kdark8276@gmail.com>
2026-06-19 13:45:37 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+import requests
+import lxml
+from smtplib import SMTP
+import os
+
+headers={
+   "Accept-Language":"en-US,en;q=0.5",
+   "User-Agent":"CCBot/2.0 (https://commoncrawl.org/faq/)"
+}
+url="https://www.amazon.in/OnePlus-Charcoal-Snapdragon%C2%AE-Personalised-Game-Changing/dp/B0FZSXYV6K/ref=sr_1_33_sspa?crid=30E8YIS7TIF4X&dib=eyJ2IjoiMSJ9.HdDcMgq7zST1Lq56dQ5wn56PlhsvVdF2b-C727YFyonGjHj071QN20LucGBJIEps.6kUhLGJ9Vrzt30dTk6Ew4VLBSg3d03kQBxJjcCWnAIg&dib_tag=se&keywords=phone%2Bunder%2B25k%2B&qid=1781855427&sprefix=phone%2Caps%2C338&xpid=FBWmj6CT96Jkm&aref=q58bZ7Pqsc&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGZfbmV4dA&th=1"
+
+response=requests.get(url,headers=headers)
+response.raise_for_status()
+website=response.text
+soup=BeautifulSoup(website,"lxml")
+
+title=soup.find("span",class_="a-size-large product-title-word-break").text
+
+price=soup.find("span",class_="a-offscreen")
+price_text=price.text
+actual_price=price_text.split("₹")[1]
+convert_to_int=int(float(actual_price.replace(",", "")))
+
+buying_price=40000
+
+if float(buying_price)>=convert_to_int:
+
+    myemail = os.environ["EMAIL"]
+    pass1 = os.environ["EMAIL_PASSWORD"]
+    receiver_email = os.environ["RECEIVER_EMAIL"]
+
+    with SMTP("smtp.gmail.com",587) as server:
+        server.starttls()
+        server.login(myemail,pass1)
+        server.sendmail(
+            from_addr=myemail,
+            to_addrs=receiver_email,
+            msg=f"Subject:Amazon Price Deals!\n\n{title}\nyour coated price ₹{buying_price} and now it is at ₹{actual_price}\n{url}".encode("utf-8")
+        )
+    print("Email sent successfully")
+else:
+    print("Price is still above your target.")
